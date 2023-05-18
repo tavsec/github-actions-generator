fc266cde3e9263e55ccdd5f481bbc2eb7e8eb0ca
Timotej Avsec <31708774+tavsec@users.noreply.github.com>
2023-05-18 18:42:00 +0000

    Prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,6 +54,6 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   router: {
-    base: '/github-actions-generator/'
-  }
+    base: '/github-actions-generator/',
+  },
 }
